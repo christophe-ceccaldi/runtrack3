b6f8b6f322d54ba2df6fdf2712ae67a1bb15cb7e
christophe-ceccaldi <christophe.ceccaldi@laplateforme.io>
2023-01-17 11:21:30 +0100

    add job07 dayofwork

--- jour01/job07/script.js
+++ jour01/job07/script.js
@@ -1,20 +1,41 @@
-function jourtravaille(){
-    for  (i = 1; i <= 151; i++) {
-      if(i % 3 === 0 && i % 5 === 0) {
-        console.log("FizzBuzz");
-      }
-      else if(i % 3 === 0) {
-        console.log("Fizz");
+function jourtravaille(dateStr){
 
-      } else if (i % 5 === 0) {
-        console.log("Buzz");
-        
-      }else {
-          console.log(i);
-        }
-      }
-    
-  }
+  const holidaysfrench = { 
+  "1,0": "jourdelan",
+  "7,3": "vendredi saint",
+  "10,3": "lundi de paques",
+  "1,4": "fête du travail",
+  "8,4": "armistice",
+ "18,4": "l'ascension",
+  "29.4":"lundi saint",
+  "14.6": "fetenationale",
+  "15,7": "jours de l'assomption",
+  "1,10": "toutsaint",
+  "11,10": "jours de l'armistice",
+  "25,11": "noel",
+ };
 
-jourtravaille();
+ var date = new Date(dateStr);
+
+ var jourdesemaine = date.getDay(); // 2
+ var jourdumois = date.getDate(); // 17
+ var moisdelannee = date.getMonth(); // 0
+ var lannee = date.getFullYear(); // 2023
+
+ var combinedDate = jourdumois + "," + moisdelannee; // "17,0"
+
+ console.log(combinedDate);
+
+ if (jourdesemaine == 6 || jourdesemaine == 0) {
+  console.log("c'est le week end");
+
+ } else if (holidaysfrench[combinedDate]){
+  console.log("c'est un jour férié");
+ }else {
+  console.log("c'est un jour de travail")
+ }
+
+
+}
+jourtravaille("2022-11-11");
   
